781e11008a676eb016e650797d7d0e104e7666be
rahul2512 <rs25.iitr@gmail.com>
2022-06-26 23:14:38 +0200

    updated

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git add IMU_input LR_analysis.txt git.sh Linear_analysis.txt Marker_output README.md analysis* *txt *py submit_jobs.sh create_job.sh heading
 git commit -m $1 
 git push git@github.com:rahul2512/MSK_ML_beta.git
-
+#
 ## git stash
 ## git pull origin main
