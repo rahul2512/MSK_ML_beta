681ed73f7201ade6c3c40eccb6e5c5954f267330
Rahul Sharma <rsharma@adminosxs-MacBook-Pro.local>
2022-06-26 22:42:19 +0200

    find best model.py

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 git add IMU_input LR_analysis.txt git.sh Linear_analysis.txt Marker_output README.md analysis* *txt *py submit_jobs.sh create_job.sh heading
-git commit -m 'updated submit_job'
+git commit -m 'find best model.py'
 git push git@github.com:rahul2512/MSK_ML_beta.git
