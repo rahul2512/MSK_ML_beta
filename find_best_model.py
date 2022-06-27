272d19aacc72155e6af199b90fcacebe6c6c45cc
rahul2512 <rs25.iitr@gmail.com>
2022-06-27 15:33:48 +0200

    update_find_best_model

--- find_best_model.py
+++ find_best_model.py
@@ -13,19 +13,17 @@ def read_k_fold_data(f):
 
 def estimate_validation_results(f):
 	data = read_k_fold_data(f)
-	avg_train_mse, avg_val_mse, avg_test_mse, avg_train_pc, avg_val_pc, avg_test_pc = [], [], [], [], [], []
+	avg_train_mse, avg_val_mse, avg_train_pc, avg_val_pc = [], [], [], []
 	for i in range(4):
 		avg_train_mse.append(data[i].iloc[0,0])	
 		avg_val_mse.append(data[i].iloc[0,1])
-		avg_test_mse.append(data[i].iloc[0,2])
 		avg_train_pc.append(data[i].iloc[1].mean())
 		avg_val_pc.append(data[i].iloc[2].mean())
-		avg_test_pc.append(data[i].iloc[3].mean())
-	return avg_train_mse, avg_val_mse, avg_test_mse, avg_train_pc, avg_val_pc, avg_test_pc
+	return avg_train_mse, avg_val_mse, avg_train_pc, avg_val_pc
 
 
 def return_model(f,criteria):
-	avg_train_mse, avg_val_mse, avg_test_mse, avg_train_pc, avg_val_pc, avg_test_pc = estimate_validation_results(f)
+	avg_train_mse, avg_val_mse,  avg_train_pc, avg_val_pc = estimate_validation_results(f)
 	if criteria == "avg_val_mse":
 		if np.mean(avg_val_mse) == 0:
 			return 1000
@@ -34,13 +32,11 @@ def return_model(f,criteria):
 
 
 def return_model_stat(f):
-	avg_train_mse, avg_val_mse, avg_test_mse, avg_train_pc, avg_val_pc, avg_test_pc = estimate_validation_results(f)
+	avg_train_mse, avg_val_mse,  avg_train_pc, avg_val_pc = estimate_validation_results(f)
 	print('avg_train_mse',np.mean(avg_train_mse))
 	print('avg_val_mse',  np.mean(avg_val_mse))
-	print('avg_test_mse', np.mean(avg_test_mse))
 	print('avg_train_pc', np.mean(avg_train_pc))
 	print('avg_val_pc',   np.mean(avg_val_pc))
-	print('avg_test_pc',  np.mean(avg_test_pc))
 	print('---------XXXXXXXXX---------XXXX--------XXXXXXXXXX--------')
 
 
@@ -56,10 +52,15 @@ def train_final_model(hyper_arg,which):
 def run_final(which, subject):
 	count=0
 	start=1000
-	for i in range(1320):
+	for i in range(43740):
 		f='text_out/stat_NN_'+which+'_'+ subject+'.hv_'+str(i)+'.CV_XXXX.txt'
-		tmp = return_model(f,"avg_val_mse")
-		print(i,tmp)
+		try:
+			tmp = return_model(f,"avg_val_mse")
+			print(tmp)
+			input()
+		except:
+			None
+#			print("index not found--", i )
 #			if tmp < start:
 #				print("model_index = ",i,start,which)
 #				start = tmp
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,4 +1,6 @@
-for i in {1..22}
+for i in {1..1216}
 do 
+echo "Submitting job ${i}"
 sbatch jobs/job_${i}.slurm 
+sleep 65
 done
