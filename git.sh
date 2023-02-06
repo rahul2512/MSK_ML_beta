6793efbe9875e225d95c2923c09498a2a1f16939
Rahul Sharma <38540112+rahul2512@users.noreply.github.com>
2023-02-06 17:37:41 +0100

    adding_RNN_codes

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git add IMU_input LR_analysis.txt git.sh Linear_analysis.txt Marker_output README.md analysis* *txt *py submit_jobs.sh create_job.sh heading
+git add IMU_input LR_analysis.txt git.sh Linear_analysis.txt Marker_output README.md analysis* *txt *py submit_jobs.sh create_job.sh heading read_in_out.py  main.py barchart_err.py
 git commit -m $1 
 git push git@github.com:rahul2512/MSK_ML_beta.git
 ## git stash
